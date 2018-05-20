2d316507215f5bc49d1521610b7698ff283e6fce
zdcthomas <zdcthomas@yahoo.com>
2018-05-19 22:29:07 -0600

    WIP implements visitor user stories

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:new,:create]
+  root "welcome#index"
+  resources :organizations, only: [:index]
+  get '/login', to: "sessions#new"
 end
--- spec/features/a_visitor_visits_the_home_page_spec.rb
+++ spec/features/a_visitor_visits_the_home_page_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe "A visitor", type: :feature do
   context 'visits the root adress' do
     describe 'displayed information' do
-      it 'should display a sign up and a login button' do
+    it 'should display a sign up and a login button' do
         visit root
 
         expect(page).to have_link("Login")
@@ -14,11 +14,11 @@ describe "A visitor", type: :feature do
         organization2 = Organization.create!(name:'another Charity', description:'does some stuff')
         organization3 = Organization.create!(name:'still yet another Charity', description:'does some more stuff')
         user = User.create!(username:'Phil',password:'anthropist')
-        review1 = user.review.create(score:3, text:'This one helped some', organization_id: organization1.id)
-        review2 = user.review.create(score:4, text:'This one helped some more', organization_id: organization2.id)
-        review3 = user.review.create(score:5, text:'This one helped way more', organization_id: organization3.id)
+        review1 = user.reviews.create(score:3, text:'This one helped some', organization_id: organization1.id)
+        review2 = user.reviews.create(score:4, text:'This one helped some more', organization_id: organization2.id)
+        review3 = user.reviews.create(score:5, text:'This one helped way more', organization_id: organization3.id)
 
-        visit root
+        visit root_path
 
         within "li:nth-child(1)" do
           expect(page).to have_content(organization3.id)
@@ -30,16 +30,24 @@ describe "A visitor", type: :feature do
           expect(page).to have_content(organization1.id)
         end
       end
+      it 'should display a link to the organizations index' do
+        visit root_path
+
+        expect(page).to have_link('Organizations')
+        click_on 'Organizations'
+
+        expect(current_path).to eq(organizations_path)
+      end
     end
     describe 'and clicks on the sign up button' do
       it 'should redirect to the sign up page' do
-        visit root
+        visit root_path
         click_on 'Sign Up'
 
         expect(current_path).to eq(new_user_path)
       end
       it 'should display a form with fields for username and password' do
-        visit root
+        visit root_path
         click_on 'Sign Up'
 
         expect(page).to have_field('user[username]')
@@ -47,7 +55,7 @@ describe "A visitor", type: :feature do
       end
       describe 'and fills out the form for signing up' do
         it 'should redirect to the root homepage if the information is valid' do
-          visit root
+          visit root_path
           click_on 'Sign Up'
 
           username = 'davebob'
