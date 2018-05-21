c5262804071aecc2d2796315e29f8948b4f944b0
zdcthomas <zdcthomas@yahoo.com>
2018-05-20 20:27:08 -0600

    implements help button

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @top_orgs = Organization.top_orgs
+    @top_organization = @top_orgs.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users, only: [:new,:create,:show]
   root "welcome#index"
-  resources :organizations, only: [:index]
+  resources :organizations, only: [:index, :show]
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   post '/logout', to: "sessions#destroy"
