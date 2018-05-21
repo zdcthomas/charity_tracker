6fb182f67e475cb08ea8e62f0f819f650729a185
zdcthomas <zdcthomas@yahoo.com>
2018-05-20 19:48:47 -0600

    adds seed file

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   gem 'pry'
   gem 'launchy'
   gem 'shoulda-matchers'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ORGANIZATIONS = ["American Red Cross", "Wounded Warrior", "Save the Children", "World Wildlife Federation", "International Rescue Commitee"]
+REVIEWS = ['another charitable organization that isnt that great', 'this one isnt great', 'a good review', 'a great review']
+USERS = ['JimBob', 'anotheruser', 'username', 'joe', 'aPlant']
+DESCRIPTIONS = ['An organization description', 'some other description', 'focuses on things overseas']
+
+ORGANIZATIONS.each do |organization|
+  Organization.create!(name:organization,
+                       description:DESCRIPTIONS.sample)
+end
+
+USERS.each do |name|
+  user = User.create!(username:name,
+                      password:'blah')
+  2.times do |num|
+    user.reviews.create!(score:num = rand(5),
+                         text:REVIEWS.sample,
+                         organization_id:Organization.all.sample.id,
+                         )
+  end
+  3.times do |num|
+    user.donations.create!(organization_id:Organization.all.sample.id,
+                           amount:num+rand(100))
+    end
+end
