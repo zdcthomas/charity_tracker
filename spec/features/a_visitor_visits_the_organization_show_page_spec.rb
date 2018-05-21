cb6a4c67113c44599264a25e17b27c1c3de932f9
zdcthomas <zdcthomas@yahoo.com>
2018-05-21 00:01:45 -0600

    TESTS organization page path

---
+++ spec/features/a_visitor_visits_the_organization_show_page_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+describe "A visitor", type: :feature do
+  describe 'visiting the organization show page' do
+    it 'should display the name and description of the organization' do
+      organization1 = Organization.create!(name:'Some Charity', description:'does some stuff')
+
+      visit organization_path organization1
+
+      expect(page).to have_content(organization1.name)
+      expect(page).to have_content(organization1.description)
+    end
+    it 'should have a donation button' do
+      organization1 = Organization.create!(name:'Some Charity', description:'does some stuff')
+
+      visit organization_path organization1
+
+      expect(page).to have_link('Donate')
+    end
+    describe 'and clicking on the the donate link' do
+      it 'should redirect to the make a dontation page' do
+        organization1 = Organization.create!(name:'Some Charity', description:'does some stuff')
+
+        visit organization_path organization1
+        click_on 'Donate'
+
+        expect(current_path).to eq(new_donation_path)
+      end
+      
+  end
+end
