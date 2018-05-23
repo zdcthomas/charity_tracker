85467b5f4340368aca7b48152368b9546eca9dce
zdcthomas <zdcthomas@yahoo.com>
2018-05-23 15:38:31 -0600

    implements pagination WIP

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,6 @@
 class OrganizationsController < ApplicationController
   def index
-    @organizations = Organization.all
+    @organizations = Organization.paginate(:page => params[:page], :per_page => 5)
   end
 
   def show 
